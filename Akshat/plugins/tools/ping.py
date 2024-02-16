5b5c7d9efb90924743bde7c6f36c794c9cc82506
Akshay Gangwar <130240533+akshayxt@users.noreply.github.com>
2024-02-16 13:06:03 +0530

    Update ping.py

--- Akshat/plugins/tools/ping.py
+++ Akshat/plugins/tools/ping.py
@@ -8,15 +8,15 @@ from Akshat.core.call import Rax
 from Akshat.utils import bot_sys_stats
 from Akshat.utils.decorators.language import language
 from Akshat.utils.inline import supp_markup
-from config import BANNED_USERS, PING_IMG_URL
+from config import BANNED_USERS, PING_VID_URL
 
 
 @app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
-    response = await message.reply_photo(
-        photo=PING_IMG_URL,
+    response = await message.reply_video(
+        video=PING_VID_URL,
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await Rax.ping()
