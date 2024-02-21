add07e4528e14b7df51f58389898d86c2a280684
Akshay Gangwar <130240533+akshayxt@users.noreply.github.com>
2024-02-21 23:22:15 +0530

    Update playlist.py

--- Akshat/plugins/play/playlist.py
+++ Akshat/plugins/play/playlist.py
@@ -21,7 +21,7 @@ from Akshat.utils.decorators.language import language, languageCB
 from Akshat.utils.inline.playlist import (botplaylist_markup,
                                               get_playlist_markup,
                                               warning_markup)
-from Akshat.utils.pastebin import VIPBin
+from Akshat.utils.pastebin import RaxBin
 import time
 import yt_dlp
 from youtube_search import YoutubeSearch
@@ -114,7 +114,7 @@ async def check_playlist(client, message: Message, _):
         count += 1
         msg += f"\n\n{count}- {title[:70]}\n"
         msg += _["playlist_5"].format(duration)
-    link = await VIPBin(msg)
+    link = await RaxBin(msg)
     lines = msg.count("\n")
     if lines >= 17:
         car = os.linesep.join(msg.split(os.linesep)[:17])
